335a92e67a3b60293ff661bda174c9a5417b6bb7
CCMPacheco <pachecoccmp@gmail.com>
2022-04-11 20:14:41 -0300

    Initial setup

---
+++ script.js
@@ -0,0 +1,32 @@
+function operate(operator, firstOperand, secondOperand) {
+  switch (operator) {
+    case "+":
+      add(firstOperand, secondOperand);
+      break;
+    case "-":
+      subtract(firstOperand, secondOperand);
+      break;
+    case "*":
+      multiply(firstOperand, secondOperand);
+      break;
+    case "/":
+      divide(firstOperand, secondOperand);
+      break;
+  }
+}
+
+function add(firstOperand, secondOperand) {
+  return firstOperand + secondOperand;
+}
+
+function subtract(firstOperand, secondOperand) {
+  return firstOperand - secondOperand;
+}
+
+function multiply(firstOperand, secondOperand) {
+  return firstOperand * secondOperand;
+}
+
+function divide(firstOperand, secondOperand) {
+  return firstOperand / secondOperand;
+}
