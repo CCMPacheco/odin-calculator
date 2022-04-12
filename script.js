c3a7c627883f4a5295b1178d68cebb519011c3fe
CCMPacheco <pachecoccmp@gmail.com>
2022-04-11 23:37:02 -0300

    Add operation functionality

--- script.js
+++ script.js
@@ -1,20 +1,68 @@
+let previousOperand = "";
+let currentOperand = "";
+let selectedOperator = null;
+
+const numberBtns = document.querySelectorAll("[data-number]");
+const operationBtns = document.querySelectorAll("[data-operation]");
+const clearBtn = document.querySelector("[data-clear]");
+const deleteBtn = document.querySelector("[data-delete]");
+const equalBtn = document.querySelector("[data-equal]");
+const previousTextElement = document.querySelector("[data-previous-operand]");
+const currentTextElement = document.querySelector("[data-current-operand]");
+
+numberBtns.forEach((numberBtn) => {
+  numberBtn.addEventListener("click", () => {
+    appendNumber(numberBtn.innerText);
+    updateDisplay();
+  });
+});
+
+operationBtns.forEach((operationBtn) => {
+  operationBtn.addEventListener("click", () => {
+    selectOperation(operationBtn.innerText);
+    updateDisplay();
+  });
+});
+
+equalBtn.addEventListener("click", () => {
+  operate(selectedOperator, previousOperand, currentOperand);
+  updateDisplay();
+});
+
+function selectOperation(operation) {
+  if (currentOperand === "") return;
+  if (previousOperand !== "") {
+    operate(selectedOperator, previousOperand, currentOperand);
+  }
+  selectedOperator = operation;
+  previousOperand = currentOperand;
+  currentOperand = "";
+}
+
 function operate(operator, firstOperand, secondOperand) {
+  let result;
+  let prev = parseFloat(firstOperand);
+  let current = parseFloat(secondOperand);
+  if (isNaN(prev) || isNaN(current)) return;
   switch (operator) {
     case "+":
-      add(firstOperand, secondOperand);
+      result = add(prev, current);
       break;
     case "-":
-      subtract(firstOperand, secondOperand);
+      result = subtract(prev, current);
       break;
     case "x":
-      multiply(firstOperand, secondOperand);
+      result = multiply(prev, current);
       break;
     case "÷":
-      divide(firstOperand, secondOperand);
+      result = divide(prev, current);
       break;
     default:
       return;
   }
+  currentOperand = result;
+  selectedOperator = null;
+  previousOperand = "";
 }
 
 function add(firstOperand, secondOperand) {
@@ -33,10 +81,12 @@ function divide(firstOperand, secondOperand) {
   return firstOperand / secondOperand;
 }
 
-const numberBtns = document.querySelectorAll("[data-number]");
-const operationBtns = document.querySelectorAll("[data-operation]");
-const clearBtn = document.querySelector("[data-clear]");
-const deleteBtn = document.querySelector("[data-delete]");
-const equalBtn = document.querySelector("[data-equal]");
-const previousTextElement = document.querySelector("[data-previous-operand]");
-const currentTextElement = document.querySelector("[data-current-operand]");
+function appendNumber(number) {
+  if (currentOperand.includes(".") && number === ".") return;
+  currentOperand = currentOperand.toString() + number.toString();
+}
+
+function updateDisplay() {
+  currentTextElement.innerText = currentOperand;
+  previousTextElement.innerText = previousOperand;
+}
